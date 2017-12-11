a679f82c8d90992eeb58e09c6962444ceb08ecb4
devonclark <dclar4@unh.newhaven.edu>
2017-12-10 22:49:00 -0500

    swapped while and try statement for failure recovery. Also added error printout to log file

--- speedbot.py
+++ speedbot.py
@@ -46,49 +46,55 @@ st = SpeedTest()   # the speed test object
 if __name__ == '__main__':
     try:
         while 1:
-            utime = time.time()
-            ping = st.ping()
-            down = st.download()/1000000.0
-            up = st.upload()/1000000.0
+            try:
+                utime = time.time()
+                ping = st.ping()
+                down = st.download()/1000000.0
+                up = st.upload()/1000000.0
 
-            logFile.write('%f,%f,%f,%f\n' % (utime, ping, down, up))
-            speedStr = 'ping: %f ms\ndown: %f Mbps\nup: %f Mbps\n' % (ping, down, up)
-            print(speedStr)
+                logFile.write('%f,%f,%f,%f\n' % (utime, ping, down, up))
+                speedStr = 'ping: %f ms\ndown: %f Mbps\nup: %f Mbps\n' % (ping, down, up)
+                print(speedStr)
 
-            lowSpeed = 0x0
-            if ping > pingLThresh:
-                lowSpeed = lowSpeed | 0x1
-            elif ping < pingHThresh:
-                lowSpeed = lowSpeed & (0xF ^ 0x1)
-            if down < downLThresh:
-                lowSpeed = lowSpeed | 0x2
-            elif down > downHThresh:
-                lowSpeed = lowSpeed & (0xF ^ 0x2)
-            if up < upLThresh:
-                lowSpeed = lowSpeed | 0x4
-            elif up > upHThresh:
-                lowSpeed = lowSpeed & (0xF ^ 0x4)
+                lowSpeed = 0x0
+                if ping > pingLThresh:
+                    lowSpeed = lowSpeed | 0x1
+                elif ping < pingHThresh:
+                    lowSpeed = lowSpeed & (0xF ^ 0x1)
+                if down < downLThresh:
+                    lowSpeed = lowSpeed | 0x2
+                elif down > downHThresh:
+                    lowSpeed = lowSpeed & (0xF ^ 0x2)
+                if up < upLThresh:
+                    lowSpeed = lowSpeed | 0x4
+                elif up > upHThresh:
+                    lowSpeed = lowSpeed & (0xF ^ 0x4)
 
-            if tweetEn:
-                status = None
-                if lowSpeed > 0 and tagISPEn:
-                    statsFmt = 'down: %.2f Mbps\nup: %.2f Mbps\nping: %.2f\nLocation: %s'
-                    commentary = '@%s, why is my internet speed so slow when I pay for %s? Fix my internet! %s'
-                    statusFmt = 'Current internet speed:\n\n' + statsFmt + '\n\n' + commentary
-                    status = statusFmt % (down, up, ping, locale, ispTwitter, plan, tags)
-                else:
-                    statsFmt = 'down: %.2f Mbps\nup: %.2f Mbps\nping: %.2f\nLocation: %s'
-                    statusFmt = 'Current internet speed:\n\n' + statsFmt
-                    status = statusFmt % (down, up, ping, locale)
+                if tweetEn:
+                    status = None
+                    if lowSpeed > 0 and tagISPEn:
+                        statsFmt = 'down: %.2f Mbps\nup: %.2f Mbps\nping: %.2f\nLocation: %s'
+                        commentary = '@%s, why is my internet speed so slow when I pay for %s? Fix my internet! %s'
+                        statusFmt = 'Current internet speed:\n\n' + statsFmt + '\n\n' + commentary
+                        status = statusFmt % (down, up, ping, locale, ispTwitter, plan, tags)
+                    else:
+                        statsFmt = 'down: %.2f Mbps\nup: %.2f Mbps\nping: %.2f\nLocation: %s'
+                        statusFmt = 'Current internet speed:\n\n' + statsFmt
+                        status = statusFmt % (down, up, ping, locale)
 
-                if status:
-                    t.statuses.update(status=status)
-                    print('Tweet sent\n')
+                    if status:
+                        t.statuses.update(status=status)
+                        print('Tweet sent\n')
 
-            time.sleep(tMin * 60)
-    except Exception as e:
-        print('* * * Exception in main * * *')
-        print(e)
+                
+            except KeyboardInterrupt:
+                raise
+            except Exception as e:
+                print('* * * Exception in main * * *')
+                print(e)
+                logFile.write('%f,-1,-1,-1,%s\n' % (utime, e))
+            finally:
+                time.sleep(tMin * 60)
     finally:
         print('Closing log file')
         logFile.close()
